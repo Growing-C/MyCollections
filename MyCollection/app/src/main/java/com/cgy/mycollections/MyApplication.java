6ae01f842077b0b3c91c4055a57b59b3a609fb60
cgy <gaoyang.chen@witontek.com>
2015-11-10 08:55:54 +0800

    init

---
+++ MyCollection/app/src/main/java/com/cgy/mycollections/MyApplication.java
@@ -0,0 +1,14 @@
+package com.cgy.mycollections;
+
+import android.app.Application;
+
+/**
+ * Created by RB-cgy on 2015/11/9.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
