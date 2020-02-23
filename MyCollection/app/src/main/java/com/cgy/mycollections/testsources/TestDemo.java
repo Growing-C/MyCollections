85ea13ec680824c0c0ee0b4f12808f298db5a063
Growing-C <cgyhard@163.com>
2020-02-23 19:38:34 +0800

    dev [modify]

--- MyCollection/app/src/main/java/com/cgy/mycollections/testsources/TestDemo.java
+++ MyCollection/app/src/main/java/com/cgy/mycollections/testsources/TestDemo.java
@@ -1,39 +1,24 @@
 package com.cgy.mycollections.testsources;
 
-import android.content.Intent;
-import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.cgy.mycollections.Demo;
-import com.cgy.mycollections.MainItemAdapter;
 import com.cgy.mycollections.R;
-import com.cgy.mycollections.functions.ui.androiddesign.recyclerview.SimpleRecyclerViewDemo;
-import com.cgy.mycollections.functions.ui.dialogdemo.DialogDemo;
-import com.cgy.mycollections.functions.ui.scale.ScaleViewDemo;
-import com.cgy.mycollections.functions.ui.systemui.statusbar.StatusBarDemo;
-import com.cgy.mycollections.functions.ui.textdemo.TextDemo;
-import com.cgy.mycollections.functions.ui.wheel.WheelDemo;
-import com.cgy.mycollections.listeners.OnItemClickListener;
+import com.cgy.mycollections.utils.SystemUiUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-//import androidx.appcompat.widget.DefaultItemAnimator;
-//import androidx.appcompat.widget.LinearLayoutManager;
-//import androidx.appcompat.widget.RecyclerView;
-
-
 public class TestDemo extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.root)
+    View mContentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +30,38 @@ public class TestDemo extends AppCompatActivity {
 
     }
 
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fullscreen:
+                SystemUiUtils.showSystemUi(mContentView);
+                break;
+            case R.id.quit_full:
+                SystemUiUtils.blurSystemUi(mContentView);
+                break;
+            default:
+                break;
+        }
+    }
+
+    void setNavVisibility(boolean visible) {
+        int newVis = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE;
+        if (!visible) {
+            newVis |= View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE;
+        }
+        // If we are now visible, schedule a timer for us to go invisible.
+        // Set the new desired visibility.
+        mContentView.setSystemUiVisibility(newVis);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 }
