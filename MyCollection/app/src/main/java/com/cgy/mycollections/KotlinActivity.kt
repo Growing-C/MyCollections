7686575574527f0595f07f895b5a2c8ea93ae171
chengaoyang <chengy4@xiaopeng.com>
2020-11-05 15:02:41 +0800

    viewbinding

--- MyCollection/app/src/main/java/com/cgy/mycollections/KotlinActivity.kt
+++ MyCollection/app/src/main/java/com/cgy/mycollections/KotlinActivity.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 //import android.support.design.widget.Snackbar
 //import android.support.v7.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatActivity
+import com.cgy.mycollections.databinding.ActivityKotlinBinding
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_kotlin.*
+//import kotlinx.android.synthetic.main.activity_kotlin.*
 
 class KotlinActivity : AppCompatActivity() {
 
+    //kotlin-android-extensions 已经deprecated了，所以不再使用其中的生成kotlin布局的方式，改用viewBinding
+    private lateinit var binding: ActivityKotlinBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_kotlin)
-        setSupportActionBar(toolbar)
-        fab.setOnClickListener { view ->
+        binding = ActivityKotlinBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        setContentView(R.layout.activity_kotlin)
+        setSupportActionBar(binding.toolbar)
+        binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
