79cc656db02c0e6255c1a83dd9937aaf9e1d9291
ChenGaoYang <chengy4@xiaopeng.com>
2020-12-02 18:31:16 +0800

    ???

--- MyCollection/testsourcelib/src/main/java/com/example/testsourcelib/TestMain.java
+++ MyCollection/testsourcelib/src/main/java/com/example/testsourcelib/TestMain.java
@@ -1,9 +1,9 @@
 package com.example.testsourcelib;
 
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Created by RB-cgy on 2018/6/15.
@@ -14,6 +14,11 @@ public class TestMain {
 //        testRound();
 //        testDirectSetOrAdd();
         getMaxNotRepeatStringLenInString("qwertyuiopqqasdfghjklzaazxcvbnmlkjzz");
+    }
+
+    public static void testCompletableFuture() {
+        CompletableFuture<String> completableFuture = new CompletableFuture<>();
+
     }
 
     /**
